352f93ec419576d6f4bd18ee495cf44a2abb2d4b
Emily Harber <emily.a.harber@gmail.com>
2019-09-24 02:18:23 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name=(my_dog_name)
+    @name = my_dog_name
+  end 
+  
+  def name 
+    @name
+  end 
+  
+  def breed=(my_dog_breed)
+    @breed = my_dog_breed
+  end 
+  
+  def breed 
+    @breed 
+  end 
+end 
